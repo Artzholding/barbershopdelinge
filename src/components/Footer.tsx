5e67d2c02177c72f3635072a5f9f50f1f2d3080c
Artzholding <75098741+Artzholding@users.noreply.github.com>
2025-10-27 20:28:21 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -140,8 +140,8 @@ export default function Footer({ setCurrentPage }: FooterProps) {
                     <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
                   </svg>
                 </div>
-                <a href="mailto:info@elst.nl" className="text-gray-400 hover:text-amber-500 transition-colors">
-                  info@elst.nl
+                <a href="mailto:Info@barbershopdelinge.nl" className="text-gray-400 hover:text-amber-500 transition-colors">
+                  Info@barbershopdelinge.nl
                 </a>
               </div>
             </div>
